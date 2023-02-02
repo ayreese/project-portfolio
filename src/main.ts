b72b8ccf99c4be0ffd409151ff8c904384cb6567
Alexis Reese <ymldnn@gmail.com>
2023-02-02 07:39:23 -0800

    skill section css added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import "./style.scss";
 import typescriptLogo from "./typescript.svg";
+const A11YSlider = require("a11y-slider");
 
-const primaryNav = document.querySelector<HTMLButtonElement>(".prim-nav");
-const navToggle =
-  document.querySelector<HTMLButtonElement>(".mobile-nav-toggle");
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  slidesToShow: 1,
+  arrows: true,
+  dots: false,
+  responsive: {
+    768: {
+      slidesToShow: 2,
+      arrows: false
+    },
+    960: {
+      disable: true // slider disabled 960px to 1279px
+    },
+    1280: {
+      disable: false,
+      slidesToShow: 4,
+      dots: true // dots enabled 1280px and up
+    }
+  }
+});
+
+slider()
+
 
 navToggle?.addEventListener("click", () => {
   primaryNav?.hasAttribute("data-visible")
@@ -12,7 +32,7 @@ navToggle?.addEventListener("click", () => {
   primaryNav?.toggleAttribute("data-visible");
   navToggle?.toggleAttribute("data-visible");
   console.log(navToggle.getAttribute("aria-expanded"));
-  console.log(navToggle?.hasAttribute("data-visible")); 
+  console.log(navToggle?.hasAttribute("data-visible"));
 });
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
