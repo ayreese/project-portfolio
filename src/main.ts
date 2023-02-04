41450f5d66c876aefd78380f0fe16b7a8efcef54
Alexis Reese <ymldnn@gmail.com>
2023-02-04 10:34:28 -0800

    skills section css correction

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,34 @@
 import "./style.scss";
-
-const A11YSlider = require("a11y-slider");
+import A11YSlider from "a11y-slider";
 
 /* mobile navigation toggle script */
-const primaryNav = document.querySelector<HTMLButtonElement>(".prim-nav");
-const navToggle =
-  document.querySelector<HTMLButtonElement>(".mobile-nav-toggle");
+const primaryNav: any = document.querySelector<HTMLButtonElement>(".prim-nav");
+const navToggle: any = document.querySelector<HTMLButtonElement>(".mobile-nav-toggle");
 
-navToggle?.addEventListener("click", () => {
-  primaryNav?.hasAttribute("data-visible")
+navToggle.addEventListener("click", (): void => {
+  primaryNav.hasAttribute("data-visible")
     ? navToggle.setAttribute("aria-expanded", "true")
     : navToggle.setAttribute("aria-expanded", "false");
-  primaryNav?.toggleAttribute("data-visible");
-  navToggle?.toggleAttribute("data-visible");
-  console.log(navToggle.getAttribute("aria-expanded"));
-  console.log(navToggle?.hasAttribute("data-visible"));
+  primaryNav.toggleAttribute("data-visible");
+  navToggle.toggleAttribute("data-visible");
 });
+/* end of mobile nav toggle script */
 
-/* Slider script */
+
+/* slider script */
 new A11YSlider(document.querySelector(".slider"), {
   slidesToShow: 1,
-  arrows: true,
-  dots: true,
+  arrows: false,
+  dots: false,
   autoplay: true,
-  autoplaySpeed: 2500,
-  autoplayHoverPause: false,
+  autoplaySpeed: 3000,
+  autoplayHoverPause: true,
+  skipBtn: false,
+  responsive: {
+    900: {
+      disable: true
+    }
+  }
 
 });
+/* end of slider script */
